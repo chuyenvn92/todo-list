f61efa7d598d33062914ec6465f690cc6dfb31e1
chuyen <maicongchuyentb@gmail.com>
2020-08-09 07:58:24 +0700

    Updates

---
+++ precache-manifest.8f3334c303cb8eafdbc6724617e87c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3f5aa3b7a8630934f26a3f6b1898ac",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "a878dc7031a2adf0b17c",
+    "url": "/todo-list/static/css/main.94a9297c.chunk.css"
+  },
+  {
+    "revision": "ab196dc095e21d35a907",
+    "url": "/todo-list/static/js/2.d53e9dca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.d53e9dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a878dc7031a2adf0b17c",
+    "url": "/todo-list/static/js/main.17c576c7.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
